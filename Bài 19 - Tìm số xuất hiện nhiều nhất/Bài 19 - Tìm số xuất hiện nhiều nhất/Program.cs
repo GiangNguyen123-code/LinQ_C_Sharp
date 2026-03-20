a8d76e87a7ec0153fd0466d7b3634e7a3bc8463c
Nguyen Do Huu Giang <nguyendohuugiang2006@gmail.com>
2026-03-20 21:54:42 +0700

    Nộp bài tập 19

---
+++ Bài 19 - Tìm số xuất hiện nhiều nhất/Bài 19 - Tìm số xuất hiện nhiều nhất/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace LythuyetCS
+{
+    class Bai16
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Họ và tên sinh viên: Nguyễn Đỗ Hữu Giang");
+            Console.WriteLine("Mã sinh viên: 2415053122312");
+            Console.WriteLine("Lớp học phần: 225LTC#02");
+            Console.WriteLine("Bài 19: Tìm số xuất hiện nhiều nhất");
+            Console.WriteLine("---------------------------------------------------------------");
+            List<int> ds = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+            Console.Write("Danh sách ban đầu: ");
+            foreach (int i in ds)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            int maxCount = ds.GroupBy(n => n).Max(g => g.Count()); 
+            var nhungSoNhieuNhat = ds.GroupBy(n => n).Where(g => g.Count() == maxCount);
+            Console.WriteLine($"Số lần xuất hiện nhiều nhất là: {maxCount} lần");
+            Console.Write("Các số xuất hiện nhiều nhất là: ");
+            foreach (var nhom in nhungSoNhieuNhat)
+            {
+                Console.Write($"{nhom.Key} "); 
+            }
+            Console.ReadLine();
+        }
+    }
+}

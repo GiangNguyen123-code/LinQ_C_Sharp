df4a1d57f8ab490981ca29ff0da9c1da843d7687
Nguyen Do Huu Giang <nguyendohuugiang2006@gmail.com>
2026-03-20 21:55:37 +0700

    Nộp bài tập 20

---
+++ Bài 20 - Top 3 sinh viên điểm cao nhất/Bài 20 - Top 3 sinh viên điểm cao nhất/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+namespace LythuyetCS
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+    class Bai20
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Họ và tên sinh viên: Nguyễn Đỗ Hữu Giang");
+            Console.WriteLine("Mã sinh viên: 2415053122312");
+            Console.WriteLine("Lớp học phần 225LTC#02");
+            Console.WriteLine("Bài 20: Top 3 sinh viên điểm cao nhất ");
+            Console.WriteLine("---------------------------------------------------------------");
+            List<Student> students = new List<Student>()
+            {
+                new Student { Id = 1, Name = "An", Score = 8 },
+                new Student { Id = 2, Name = "Binh", Score = 6 },
+                new Student { Id = 3, Name = "Chi", Score = 9 },
+                new Student { Id = 4, Name = "Dung", Score = 7 }
+            };
+            var sapxep = students.OrderByDescending(sv => sv.Score).Take(3);
+            foreach (Student st in sapxep)
+            {
+                Console.WriteLine($"Id sinh viên: {st.Id}, Tên sinh viên: {st.Name}, Điểm số: {st.Score}");
+            }
+            Console.ReadLine();
+        }
+    }
+}

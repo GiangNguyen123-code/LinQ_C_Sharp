159fb6303606fcd4b0048634b0663a526f658722
Nguyen Do Huu Giang <nguyendohuugiang2006@gmail.com>
2026-03-20 21:44:09 +0700

    Nộp bài tập 9

---
+++ Bài 9 - Lấy danh sách tên sinh viên/Bài 9 - Lấy danh sách tên sinh viên/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace LythuyetCS
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+    class Bai9
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Họ và tên sinh viên: Nguyễn Đỗ Hữu Giang");
+            Console.WriteLine("Mã sinh viên: 2415053122312");
+            Console.WriteLine("Lớp học phần 225LTC#02");
+            Console.WriteLine("Bài 9: Lấy danh sách tên sinh viên ");
+            Console.WriteLine("---------------------------------------------------------------");
+            List<Student> students = new List<Student>()
+            {
+                new Student { Id = 1, Name = "An", Score = 8 },
+                new Student { Id = 2, Name = "Binh", Score = 6 },
+                new Student { Id = 3, Name = "Chi", Score = 9 },
+                new Student { Id = 4, Name = "Dung", Score = 7 }
+            };
+            var tensv = students.Select(p => p.Name);
+            foreach (var s in tensv)
+            {
+                Console.WriteLine($"Tên sinh viên: {s}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
